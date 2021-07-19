05da43e646a7f6a01e7993497eba56ab0512c247
josue rodriguez <joshvoll@yahoo.com>
2021-07-19 11:11:11 -0600

    adding server resolution

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,22 @@ use actix_web::{
 };
 use std::net::TcpListener;
 
+// FormData represent the data from the post request
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+
+
 // health_check is going to return a status code HTTP 200
 async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+// subscribe access the subscription for users
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -18,10 +32,11 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
 
-    Ok(server)
+    Ok(server) 
 }
 
