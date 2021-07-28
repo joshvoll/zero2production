a2d64f97541e42726f297760cb2c14ee6febeaa0
josue rodriguez <joshvoll@yahoo.com>
2021-07-27 18:53:18 -0600

    adding log, telemtry and subscriptions scan

--- src/startup.rs
+++ src/startup.rs
@@ -6,6 +6,7 @@ use actix_web::{
     App,
     HttpServer
 };
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -20,6 +21,7 @@ pub fn run(
     let db_pool = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
